6dcc841f3a69b6ca698c7b99f6370eb1745618b8
yinmengting <yinmengting@ehousechina.com>
2017-03-06 17:23:23 +0800

    4-2

---
+++ cpp/exercise/4-2.cpp
@@ -0,0 +1,31 @@
+/* students please write your program here */
+
+#include <iostream>
+using namespace std;
+ 
+class Integer{
+private:
+    int _num;
+//getLength()函数获取_num长度
+    int getLength(){
+    }
+public:
+//Integer类构造函数
+    Integer(int num):_num(num){
+    }
+//反转_num
+    int inversed(){
+        int num = 0;
+        do {
+            int single = _num%10;
+            num = num * 10 + single;
+        } while (num /= 10 > 0)
+    }
+};
+ 
+int main() {
+    int n;
+    cin >> n;
+    Integer integer(n);
+    cout << integer.inversed() << endl;
+}
