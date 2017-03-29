6b9a12af7ab26e5e0139fa2ce94fdaba1997e16b
ViolaMiki <mikizui29@gmail.com>
2017-03-29 16:47:54 +0800

    lis

--- dataStructure/list/list_implementation.h
+++ dataStructure/list/list_implementation.h
@@ -1,3 +1,6 @@
 #pragma once
 
 #include "list_init.h"
+#include "list_find.h"
+#include "list_insert.h"
+#include "list_operator.h"
--- dataStructure/list/list_insert.h
+++ dataStructure/list/list_insert.h
@@ -2,5 +2,20 @@
 
 template <typename T>
 ListNodePosi(T) insertAsFirst(T const& e) {
-    _size++; first.insertAsPred(e);
+    _size++;return header->insertAsPred(e);
+}
+
+template <typename T>
+ListNodePosi(T) insertAsLast(T const& e) {
+    _size++;return trailer->insertAsSucc(e);
+}
+
+template <typename T>
+ListNodePosi(T) insertA(ListNodePosi(T) p, T const& e) {
+    _size++;return p->insertAsSucc(e);
+}
+
+template <typename T>
+ListNodePosi(T) insertB(ListNodePosi(T) p, T const& e) {
+    _size++;return p->insertAsPred(e);
 }
